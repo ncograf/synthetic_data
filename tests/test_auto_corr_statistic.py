930aa33013c6017c93442ff78927d0dd02359c58
Nico Graf <ncograf@gmail.com>
2024-03-24 08:34:21 +0100

    Update tests

--- tests/test_auto_corr_statistic.py
+++ tests/test_auto_corr_statistic.py
@@ -88,7 +88,27 @@ class TestAutoCorrStatistic:
         base_stat.set_statistics(None)
         assert np.array_equal(base_stat.statistic.round(6),np.array([[-1,1,-1,1],[-1,1,-1,1],[-1,1,-1,1],[-1,1,-1,1]]).T, equal_nan=True)
 
+    def test_get_statistic_boosted(self):
+        
+        log_stat = lstat.LogReturnStatistic(0.001)
+        base_stat = stat.AutoCorrStatistic(max_lag=4, underlaying=log_stat, implementation='boosted')
+        _symbols = ["a", "b", "c", "d"]
+        _returns = np.array([2,1,2,1,2,1,2,1,2,1])
+        n_dates = _returns.shape[0] + 1
+        _returns = _returns
+        _prices = np.ones(n_dates)
+        for i in range(len(_returns)):
+            _prices[i+1] = _prices[i] * _returns[i]
+        _data = np.einsum('kl,k->kl',np.ones((n_dates, 4)), _prices)
+        _data[:4,1] = np.nan
+        _dates = [pd.Timestamp(f'2017-01-01') + pd.Timedelta(days=i) for i in range(n_dates)]
+        df = pd.DataFrame(_data, columns=_symbols, index=_dates)
+        log_stat.set_statistics(df)
+        base_stat.set_statistics(None)
+        assert np.array_equal(base_stat.statistic.round(6),np.array([[-1,1,-1,1],[-1,1,-1,1],[-1,1,-1,1],[-1,1,-1,1]]).T, equal_nan=True)
+
 if __name__ == '__main__':
     TestAutoCorrStatistic().test_get_statistic()
     TestAutoCorrStatistic().test_get_statistic_simple()
+    TestAutoCorrStatistic().test_get_statistic_simple_boosted()
     TestAutoCorrStatistic().test_get_statistic_python_loop()
