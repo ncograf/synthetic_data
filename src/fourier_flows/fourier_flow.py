Fix fourier flow training with normalization

src/fourier_flows/fourier_flow.py
@@ -8,7 +8,14 @@ from torch.distributions.multivariate_normal import MultivariateNormal
 
 
 class FourierFlow(nn.Module):
-    def __init__(self, hidden_dim: int, D: int, T: int, n_layer: int):
+    def __init__(
+        self,
+        hidden_dim: int,
+        D: int,
+        T: int,
+        n_layer: int,
+        dtype: torch.dtype = torch.float64,
+    ):
         """Fourier Flow network for one dimensional time series
 
         Args:
@@ -16,6 +23,7 @@ class FourierFlow(nn.Module):
             D (int): Sample set size
             T (int): Time series size
             n_layer (int): number of spectral layers to be used
+            dtype (torch.dtype, optional): type of data. Defaults to torch.float64.
         """
 
         nn.Module.__init__(self)
@@ -24,22 +32,32 @@ class FourierFlow(nn.Module):
         self.T = T
         self.hidden_dim = hidden_dim
         self.n_layer = n_layer
+        self.dtype = dtype
 
         self.latent_size = T // 2 + 1
-        mu = torch.zeros(2 * self.latent_size)
-        sigma = torch.eye(2 * self.latent_size)
+        mu = torch.zeros(2 * self.latent_size, dtype=self.dtype)
+        sigma = torch.eye(2 * self.latent_size, dtype=self.dtype)
 
         self.dist_z = MultivariateNormal(mu, sigma)
 
         self.layers = nn.ModuleList(
             [
-                SpectralFilteringLayer(D=self.D, T=self.T, hidden_dim=self.hidden_dim)
+                SpectralFilteringLayer(
+                    D=self.D, T=self.T, hidden_dim=self.hidden_dim, dtype=dtype
+                )
                 for _ in range(self.n_layer)
             ]
         )
         self.flips = [True if i % 2 else False for i in range(self.n_layer)]
 
         self.dft = FourierTransformLayer(T=self.T)
+        self.dft_scale = 1
+        self.dft_shift = 0
+
+    def set_normilizing(self, X: torch.Tensor):
+        x_fft = self.dft(X)
+        self.dft_shift = torch.mean(x_fft)
+        self.dft_scale = torch.std(x_fft)
 
     def forward(
         self, x: torch.Tensor
@@ -54,6 +72,8 @@ class FourierFlow(nn.Module):
         """
 
         x_fft: torch.Tensor = self.dft(x)
+
+        x_fft = (x_fft - self.dft_shift) / self.dft_scale
 
         log_jac_dets = []
         for layer, f in zip(self.layers, self.flips):
@@ -85,6 +105,8 @@ class FourierFlow(nn.Module):
 
         for layer, f in zip(reversed(self.layers), reversed(self.flips)):
             z_complex = layer.inverse(z_complex, flip=f)
+
+        z_complex = z_complex * self.dft_scale + self.dft_shift
 
         x = self.dft.inverse(z_complex)
 

src/fourier_flows/fourier_flow_generator.py
@@ -11,9 +11,17 @@ from torch.utils.data import DataLoader, TensorDataset
 class FourierFlowGenerator(base_generator.BaseGenerator):
     def __init__(
         self,
-        name="FourierFlow",
+        learning_rate: float,
+        epochs: int,
+        name: str = "FourierFlow",
+        dtype: torch.dtype = torch.float64,
     ):
         base_generator.BaseGenerator.__init__(self, name)
+        self.data_min = 0
+        self.data_amplitude = 1
+        self.learning_rate = learning_rate
+        self.epochs = epochs
+        self.dtype = dtype
 
     def fit_model(self, price_data: npt.ArrayLike, lag: int = 1, seq_len: int = 101):
         """Fit a Fourier Flow Neural Network. I.e. Train the network on the given data.
@@ -30,25 +38,22 @@ class FourierFlowGenerator(base_generator.BaseGenerator):
         if price_data.ndim != 1:
             raise ValueError("Input price data must be one dimensional")
 
-        data = np.array(price_data)
-        data_mask = ~np.isnan(data)
+        data = torch.tensor(price_data, dtype=self.dtype)
+        data_mask = ~torch.isnan(data)
         data = data[
             data_mask
         ]  # drop the nans TODO, this might give wrong results for missing nans
         self._zero_price = data[0]
-        log_returns = np.log(data[1:] / data[:-1])
+        log_returns = torch.log(data[1:] / data[:-1])
 
         # split the data
         X = np.lib.stride_tricks.sliding_window_view(
             log_returns, seq_len, axis=0, writeable=False
         )
-        X = torch.tensor(X, dtype=torch.float32)
-
-        n_epochs = 10
-        learning_rate = 1e-3
+        X = torch.tensor(X)
 
         self._model, self._losses = self.train_fourier_flow(
-            X=X, epochs=n_epochs, learning_rate=learning_rate
+            X=X, epochs=self.epochs, learning_rate=self.learning_rate
         )
 
     def model(self) -> FourierFlow:
@@ -65,17 +70,36 @@ class FourierFlowGenerator(base_generator.BaseGenerator):
 
         n = len // self._model.T + 1
 
-        return_simulation = self._model.sample(n)
-        return_simulation = return_simulation.detach().numpy().flatten()
-        return_simulation = return_simulation[:len]
+        model_output = self._model.sample(n)
+        log_returns = (model_output * self.data_amplitude) + self.data_min
+        log_returns = log_returns.detach().numpy().flatten()
+        return_simulation = np.exp(log_returns[:len])
 
-        price_simulation = np.zeros_like(return_simulation, dtype=np.float64)
+        price_simulation = np.zeros_like(return_simulation, dtype=np.float32)
         price_simulation[0] = self._zero_price
 
-        for i in range(1, price_simulation.shape[0]):
-            price_simulation[i] = price_simulation[i - 1] * return_simulation[i]
+        for i in range(0, price_simulation.shape[0] - 1):
+            price_simulation[i + 1] = price_simulation[i] * return_simulation[i]
 
         return (price_simulation, return_simulation)
+
+    def min_max_scaling(self, data: torch.Tensor) -> torch.Tensor:
+        """Min max scaling of data
+
+        Args:
+            data (torch.Tensor): Data to be scaled
+
+        Returns:
+            torch.Tensor: scaled data
+        """
+
+        self.data_min = torch.min(data)
+        data_ = data - self.data_min
+        self.data_amplitude = torch.max(data_)
+
+        data_ = data_ / self.data_amplitude
+
+        return data_
 
     def train_fourier_flow(
         self, X: torch.Tensor, epochs: int, learning_rate: float
@@ -85,13 +109,19 @@ class FourierFlowGenerator(base_generator.BaseGenerator):
         hidden_dim = T * 2
         n_layer = 10
 
-        model = FourierFlow(hidden_dim=hidden_dim, D=D, T=T, n_layer=n_layer)
+        model = FourierFlow(
+            hidden_dim=hidden_dim, D=D, T=T, n_layer=n_layer, dtype=self.dtype
+        )
 
-        dataset = TensorDataset(X)
+        X_scaled = self.min_max_scaling(X)
+
+        model.set_normilizing(X_scaled)
+
+        dataset = TensorDataset(X_scaled)
         loader = DataLoader(dataset=dataset, batch_size=128)
 
         optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
-        scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.999)
+        scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.995)
 
         losses = []
 
@@ -104,11 +134,14 @@ class FourierFlowGenerator(base_generator.BaseGenerator):
                 loss = torch.mean(-log_prob_z - log_jac_det)
 
                 loss.backward()
+
+                # torch.nn.utils.clip_grad_norm_(model.parameters(), 1)
+
                 optimizer.step()
-                scheduler.step()
 
                 epoch_loss += loss.item()
 
+            scheduler.step()
             epoch_loss / len(loader)
 
             losses.append(epoch_loss)
@@ -116,7 +149,7 @@ class FourierFlowGenerator(base_generator.BaseGenerator):
             if epoch % 10 == 0:
                 print(
                     (
-                        f"Epoch: {epoch:>10d}, last loss {loss.item():>10.4f},"
+                        f"Epoch: {epoch:>10d}, last loss {epoch_loss:>10.4f},"
                         f"aveage_loss {np.mean(losses):>10.4f}"
                     )
                 )

src/fourier_flows/spectral_filtering_layer.py
@@ -5,7 +5,9 @@ import torch.nn as nn
 
 
 class SpectralFilteringLayer(nn.Module):
-    def __init__(self, D: int, T: int, hidden_dim: int):
+    def __init__(
+        self, D: int, T: int, hidden_dim: int, dtype: torch.dtype = torch.float64
+    ):
         """Spectral filtering layer for seqences
 
         see https://arxiv.org/abs/1605.08803 for implementation details
@@ -18,27 +20,31 @@ class SpectralFilteringLayer(nn.Module):
 
         nn.Module.__init__(self)
 
+        self.count = 0
+
         self.D = D
         self.split_size = T // 2 + 1
+        self.dtype = dtype
 
         self.H_net = nn.Sequential(
-            nn.Linear(self.split_size, hidden_dim),
-            nn.BatchNorm1d(hidden_dim),
+            nn.Linear(self.split_size, hidden_dim, dtype=self.dtype),
+            nn.LayerNorm(hidden_dim, dtype=self.dtype),
             nn.Sigmoid(),
-            nn.Linear(hidden_dim, hidden_dim),
-            nn.BatchNorm1d(hidden_dim),
+            nn.Linear(hidden_dim, hidden_dim, dtype=self.dtype),
+            nn.LayerNorm(hidden_dim, dtype=self.dtype),
             nn.Sigmoid(),
-            nn.Linear(hidden_dim, self.split_size),
+            nn.Linear(hidden_dim, self.split_size, dtype=self.dtype),
+            nn.Tanh(),
         )
 
         self.M_net = nn.Sequential(
-            nn.Linear(self.split_size, hidden_dim),
-            nn.BatchNorm1d(hidden_dim),
+            nn.Linear(self.split_size, hidden_dim, dtype=self.dtype),
+            nn.LayerNorm(hidden_dim, dtype=self.dtype),
             nn.Sigmoid(),
-            nn.Linear(hidden_dim, hidden_dim),
-            nn.BatchNorm1d(hidden_dim),
+            nn.Linear(hidden_dim, hidden_dim, dtype=self.dtype),
+            nn.LayerNorm(hidden_dim, dtype=self.dtype),
             nn.Sigmoid(),
-            nn.Linear(hidden_dim, self.split_size),
+            nn.Linear(hidden_dim, self.split_size, dtype=self.dtype),
         )
 
     def forward(self, x: torch.Tensor, flip: bool) -> Tuple[torch.Tensor, torch.Tensor]: