3f5cb313354c42c08771f282281f4ab6924b67df
Nico Graf <ncograf@gmail.com>
2024-04-30 13:51:35 +0200

    Add HPC run script for fourier flow

---
+++ scripts/hpc_run_scripts/run_fourier_flow_generator.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#SBATCH --ntasks=1
+#SBATCH --gpus=rtx_4090:1
+#SBATCH --mem-per-cpu=2G
+#SBATCH --time=30:00
+#SBATCH --output=run_output.out
+#SBATCH --error=run_error.out
+
+module load eth_proxy
+
+poetry run python scripts/train_fourier_flows.py --learning-rate 1e-2 --gamma 0.999 --epochs 1000 --batch-size 128 --hidden-dim 200 --num-layer 10 --lag 1 --seq-len 101 --dtype float32 --symbol MSFT
