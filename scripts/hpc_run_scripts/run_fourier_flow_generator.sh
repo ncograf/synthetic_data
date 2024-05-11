43d8ff919c9c20d2902bd34d269f2c38c896fbdc
Nico Graf <ncograf@gmail.com>
2024-05-11 16:12:55 +0200

    Change experiments slighly

--- scripts/hpc_run_scripts/run_fourier_flow_generator.sh
+++ scripts/hpc_run_scripts/run_fourier_flow_generator.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --ntasks=1
-## #SBATCH --gpus=rtx_4090:1
+##SBATCH --gpus=rtx_4090:1
 #SBATCH --mem-per-cpu=1G
 #SBATCH --time=4:00:00
 #SBATCH --output=run_output.out
@@ -9,4 +9,4 @@
 
 module load eth_proxy
 
-poetry run accelerate launch --config_file accelerate.yaml scripts/train_fourier_flows.py --learning-rate 1e-3 --gamma 0.999 --epochs 500 --batch-size 256 --hidden-dim 150 --num-layer 4 --lag 1 --seq-len 301 --dtype float32 -s MSFT -s AMZN -s TSLA -s AAPL -s IBM -s GS -s BRO -s BLK -s GOOGL
+poetry run accelerate launch --config_file accelerate.yaml scripts/train_fourier_flows.py --learning-rate 1e-3 --gamma 0.995 --epochs 700 --batch-size 512 --hidden-dim 200 --num-layer 8 --lag 1 --seq-len 501 --dtype float32 -s MSFT -s AMZN -s TSLA -s AAPL -s IBM -s GS -s BRO -s BLK -s GOOGL
