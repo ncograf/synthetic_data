dc9c092077a3b9139335be2a3caa67f5cf044ccb
Nico Graf <ncograf@gmail.com>
2024-05-11 13:23:45 +0200

    Add accelerate yaml and adapt hpc run script

--- scripts/hpc_run_scripts/run_fourier_flow_generator.sh
+++ scripts/hpc_run_scripts/run_fourier_flow_generator.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #SBATCH --ntasks=1
-#SBATCH --gpus=rtx_4090:1
-#SBATCH --mem-per-cpu=2G
-#SBATCH --time=30:00
+## #SBATCH --gpus=rtx_4090:1
+#SBATCH --mem-per-cpu=1G
+#SBATCH --time=4:00:00
 #SBATCH --output=run_output.out
 #SBATCH --error=run_error.out
 
 module load eth_proxy
 
-poetry run python scripts/train_fourier_flows.py --learning-rate 1e-2 --gamma 0.999 --epochs 100 --batch-size 128 --hidden-dim 200 --num-layer 10 --lag 1 --seq-len 101 --dtype float32 --symbols MSFT
+poetry run accelerate launch --config_file accelerate.yaml scripts/train_fourier_flows.py --learning-rate 1e-3 --gamma 0.999 --epochs 500 --batch-size 256 --hidden-dim 150 --num-layer 4 --lag 1 --seq-len 301 --dtype float32 -s MSFT -s AMZN -s TSLA -s AAPL -s IBM -s GS -s BRO -s BLK -s GOOGL
