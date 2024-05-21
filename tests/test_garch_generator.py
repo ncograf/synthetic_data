5bd085097537d804599bc3b9aa933a22f1cd9c96
Nico Graf <ncograf@gmail.com>
2024-05-21 17:09:01 +0200

    Add tests for new GARCH models

--- tests/test_garch_generator.py
+++ tests/test_garch_generator.py
@@ -1,4 +1,7 @@
-import garch_generator
+from pathlib import Path
+
+import garch_index_generator
+import garch_univar_generator
 import real_data_loader as data
 
 
@@ -8,12 +11,12 @@ class TestGarchGenerator:
     def test_fit_model(self):
         data_loader = data.RealDataLoader()
         data_dict, error_dict = data_loader._get_local_data(
-            ["SPY"],
+            ["SPY", "WAB"],
             "tests/testdata/local",
             required_columns=["Adj Close", "Close", "Open"],
         )
 
-        assert len(data_dict) == 1
+        assert len(data_dict) == 2
         assert error_dict == {}
 
         for k in data_dict.keys():
@@ -25,18 +28,68 @@ class TestGarchGenerator:
 
         # fit close dates
         data_ = data_dict["SPY"].loc[:, "Close"]
-        model = garch_generator.GarchGenerator()
-        model.fit_model(data_)
+        generator = garch_univar_generator.GarchUnivarGenerator()
+        config = {"p": 1, "q": 1, "dist": "normal"}
+        model_config = generator.fit_model(data_, config)
+        price, returns = generator.sample(100, 20, model_config)
 
-        try:
-            model.check_model()
-        except:  # noqa E722
-            assert False, "No error should be raised as the model is set"
+        assert price.shape[0] == 101
+        assert returns.shape[0] == 100
 
-        gen_price, gen_ret = model.generate_data(600, 200)
-        assert gen_price.shape[0] == 600
-        assert gen_ret.shape[0] == 600
+    def test_student(self):
+        data_loader = data.RealDataLoader()
+        data_dict, error_dict = data_loader._get_local_data(
+            ["SPY", "WAB"],
+            "tests/testdata/local",
+            required_columns=["Adj Close", "Close", "Open"],
+        )
+
+        assert len(data_dict) == 2
+        assert error_dict == {}
+
+        for k in data_dict.keys():
+            assert data_dict[k].index.name == "Date"
+        assert len(data_dict["SPY"]) == 7828
+        assert set(data_dict["SPY"].columns) == set(
+            ["Open", "High", "Low", "Close", "Adj Close", "Volume"]
+        )
+
+        # fit close dates
+        data_ = data_dict["SPY"].loc[:, "Close"]
+        generator = garch_univar_generator.GarchUnivarGenerator()
+        config = {"p": 4, "q": 2, "dist": "studentt"}
+        model_config = generator.fit_model(data_, config)
+        price, returns = generator.sample(100, 20, model_config)
+
+        assert price.shape[0] == 101
+        assert returns.shape[0] == 100
+
+    def test_index_simple(self):
+        data_loader = data.RealDataLoader(cache="tests/testdata/cache")
+        real_data = data_loader.get_timeseries(data_path="tests/testdata/local")
+        real_data = real_data.loc[:, ["AMZN", "GOOG", "WAB"]]
+
+        generator = garch_index_generator.GarchIndexGenerator()
+        garch_config = {"q": 2, "p": 2, "dist": "normal"}
+        training_config = {"garch_config": garch_config}
+        metadata = generator.fit(
+            real_data,
+            training_conifg=training_config,
+            cache="tests/testdata/cache",
+            seed=10,
+        )
+        samples = generator.sample(
+            n_samples=100, metadata=metadata, cache="tests/testdata/cache", seed=10
+        )
+
+        assert samples[0].shape[0] == 101
+        assert samples[0].shape[1] == 3
+        assert set(samples[0].columns) == {"AMZN", "GOOG", "WAB"}
+        assert samples[1].shape[0] == 100
+
+        for p in Path("tests/testdata/cache").glob("*.pt"):
+            p.unlink()
 
 
 if __name__ == "__main__":
-    TestGarchGenerator().test_fit_model()
+    TestGarchGenerator().test_index_simple()
---
+++ tests/test_univar_garch_model.py
@@ -0,0 +1,74 @@
+import numpy as np
+import univar_garch_model
+from accelerate.utils import set_seed
+
+
+class TestUnivarGarchModel:
+    """Test Function to get local data"""
+
+    def test_sample(self):
+        model_dict = {"mu": 0, "omega": 0, "alpha": [0], "beta": [0], "dist": "normal"}
+        initial_price = 0
+        model = univar_garch_model.UnivarGarchModel(model_dict, initial_price)
+
+        s = model.sample(2, 0)
+
+        assert len(s) == 2
+        assert len(s[0]) == 3
+        assert len(s[1]) == 2
+
+    def test_more_q(self):
+        model_dict = {
+            "mu": 0.1,
+            "omega": 0.1,
+            "alpha": [0, 0.2],
+            "beta": [0, 0.4, 0.4],
+            "dist": "normal",
+        }
+        initial_price = 0
+        model = univar_garch_model.UnivarGarchModel(model_dict, initial_price)
+
+        set_seed(20)
+        s = model.sample(4, 0)
+
+        assert len(s) == 2
+        assert len(s[0]) == 5
+        assert len(s[1]) == 4
+
+        model_dict = {
+            "mu": 0.1,
+            "omega": 0.1,
+            "alpha": [0, 0.2],
+            "beta": [0, 0.3, 0.4],
+            "dist": "normal",
+        }
+        model = univar_garch_model.UnivarGarchModel(model_dict, initial_price)
+
+        set_seed(20)
+        ss = model.sample(4, 0)
+        assert np.all(ss[1][:2] == s[1][:2])
+        assert np.all(ss[1][2:] != s[1][2:])
+
+    def test_more_dist(self):
+        set_seed(20)
+
+        model_dict = {
+            "mu": 0.1,
+            "omega": 0.1,
+            "alpha": [0, 0.2],
+            "beta": [0, 0.4, 0.4],
+            "dist": "studentt",
+            "nu": 3,
+        }
+        initial_price = 0
+        model = univar_garch_model.UnivarGarchModel(model_dict, initial_price)
+
+        s = model.sample(4, 0)
+
+        assert len(s) == 2
+        assert len(s[0]) == 5
+        assert len(s[1]) == 4
+
+
+if __name__ == "__main__":
+    TestUnivarGarchModel().test_more_q()
