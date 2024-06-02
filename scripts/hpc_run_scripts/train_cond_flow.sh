a0a62e15552e141e3ecb635eb4d091479eac1649
Nico Graf <ncograf@gmail.com>
2024-06-02 15:16:59 +0200

    Add more memory and fix scripts on euler

--- scripts/hpc_run_scripts/train_cond_flow.sh
+++ scripts/hpc_run_scripts/train_cond_flow.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --ntasks=1
 #SBATCH --gpus=rtx_4090:1
-#SBATCH --mem-per-cpu=1500M
+#SBATCH --mem-per-cpu=3072M
 #SBATCH --time=05:00:00
 #SBATCH --output=run_output.out
 #SBATCH --error=run_error.out
--- scripts/hpc_run_scripts/train_time_gan.sh
+++ scripts/hpc_run_scripts/train_time_gan.sh
@@ -2,11 +2,11 @@
 
 #SBATCH --ntasks=1
 #SBATCH --gpus=rtx_4090:1
-#SBATCH --mem-per-cpu=2000M
-#SBATCH --time=10:00:00
+#SBATCH --mem-per-cpu=4096M
+#SBATCH --time=20:00:00
 #SBATCH --output=run_output.out
 #SBATCH --error=run_error.out
 
 module load eth_proxy
 
-poetry run accelerate launch --config_file accelerate.yaml --no_python ./scripts/start_agent.sh -n 1 5xktctnc
+poetry run accelerate launch --config_file accelerate.yaml --no_python ./scripts/start_agent.sh -n 1 8vfjbnda
