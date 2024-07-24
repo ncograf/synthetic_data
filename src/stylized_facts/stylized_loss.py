1fec113f7e40f8e9d37dba9d44414d6e296658aa
Nico Graf <ncograf@gmail.com>
2024-07-24 18:25:17 +0200

    Fix stylized_losses and improve stylized scores

--- src/stylized_facts/stylized_loss.py
+++ src/stylized_facts/stylized_loss.py
@@ -6,36 +6,54 @@ import volatility_clustering
 
 
 def lu_loss(syn: torch.Tensor):
-    lin_upred = linear_unpredictability.linear_unpredictability_torch(syn, 1000)
+    syn = syn.transpose(0, 2).squeeze(1)
+    lin_upred = linear_unpredictability.linear_unpredictability(syn, 100)
     return torch.mean(lin_upred**2)
 
 
 def vc_loss(syn: torch.Tensor, real: torch.Tensor):
-    syn_vc = torch.mean(
-        volatility_clustering.volatility_clustering_torch(syn, 500), dim=1
-    )
-    real_vc = torch.mean(
-        volatility_clustering.volatility_clustering_torch(real, 500), dim=1
-    )
+    syn = syn.transpose(0, 2).squeeze(1)
+    real = real.transpose(0, 2).squeeze(1)
+    syn_vc = torch.mean(volatility_clustering.volatility_clustering(syn, 150), dim=1)
+    real_vc = torch.mean(volatility_clustering.volatility_clustering(real, 150), dim=1)
 
     return torch.mean((syn_vc - real_vc) ** 2)
 
 
 def le_loss(syn: torch.Tensor, real: torch.Tensor):
-    syn_le = torch.mean(leverage_effect.leverage_effect_torch(syn, max_lag=100), dim=1)
-    real_le = torch.mean(
-        leverage_effect.leverage_effect_torch(real, max_lag=100), dim=1
-    )
+    syn = syn.transpose(0, 2).squeeze(1)
+    real = real.transpose(0, 2).squeeze(1)
+    syn_le = torch.mean(leverage_effect.leverage_effect(syn, max_lag=80), dim=1)
+    real_le = torch.mean(leverage_effect.leverage_effect(real, max_lag=80), dim=1)
 
     return torch.mean((syn_le - real_le) ** 2)
 
 
 def cf_loss(syn: torch.Tensor, real: torch.Tensor):
+    syn = syn.transpose(0, 2).squeeze(1)
+    real = real.transpose(0, 2).squeeze(1)
     syn_le = torch.mean(
-        coarse_fine_volatility.coarse_fine_volatility_torch(syn, max_lag=100)[0], dim=1
+        coarse_fine_volatility.coarse_fine_volatility(syn, max_lag=50, tau=5)[0], dim=1
     )
     real_le = torch.mean(
-        coarse_fine_volatility.coarse_fine_volatility_torch(real, max_lag=100)[0], dim=1
+        coarse_fine_volatility.coarse_fine_volatility(real, max_lag=50, tau=5)[0], dim=1
     )
 
     return torch.mean((syn_le - real_le) ** 2)
+
+
+if __name__ == "__main__":
+    import load_data
+
+    sp500 = load_data.load_log_returns("sp500")
+    data_a = torch.tensor(sp500[:2048, :10]).T.unsqueeze(1)
+    data_b = torch.tensor(sp500[:2048, 10:20]).T.unsqueeze(1)
+    c = torch.ones_like(data_b, requires_grad=True)
+
+    loss = cf_loss(data_a, data_b * c)
+    loss += le_loss(data_a, data_b * c)
+    loss += lu_loss(data_b * c)
+    loss += vc_loss(data_a, data_b * c)
+    loss.backward()
+
+    print(loss)
--- src/stylized_facts/stylized_score.py
+++ src/stylized_facts/stylized_score.py
@@ -1,5 +1,5 @@
 import warnings
-from typing import Callable, Tuple
+from typing import List, Tuple
 
 import bootstrap
 import coarse_fine_volatility
@@ -14,50 +14,11 @@ import volatility_clustering
 from scipy.stats import wasserstein_distance
 
 
-def _stf_score(
-    stf: Callable,
-    real_data: npt.ArrayLike,
-    syn_data: npt.ArrayLike,
-    B: int,
-    S: int,
-    L: int,
-    **kwargs,
-) -> Tuple[npt.ArrayLike, float]:
-    real_data = np.asarray(real_data)
-    syn_data = np.asarray(syn_data)
-
-    # two dimensional data needs to be boostrapped
-    if real_data.ndim == 2:
-        real_data, _ = bootstrap.boostrap_distribution(
-            real_data, stf, B, S, L, **kwargs
-        )
-    else:
-        assert real_data.ndim == 3
-        real_data = np.asarray([np.mean(stf(d, **kwargs), axis=1) for d in real_data]).T
-
-    if syn_data.ndim == 2:
-        dist_b, _ = bootstrap.boostrap_distribution(syn_data, stf, B, S, L, **kwargs)
-    else:
-        assert syn_data.ndim == 3
-        dist_b = np.asarray([np.mean(stf(d, **kwargs), axis=1) for d in syn_data]).T
-
-    n = real_data.shape[0]
-    assert dist_b.shape[0] == real_data.shape[0]
-
-    w_dist = []
-    for i in range(n):
-        std = np.std(real_data[i, :])
-        w_dist.append(wasserstein_distance(real_data[i, :] / std, dist_b[i, :] / std))
-
-    return np.asarray(w_dist), np.mean(w_dist)
-
-
-def stylized_score(
-    real_data: npt.ArrayLike, syn_data: npt.ArrayLike, B: int, S: int, L: int
-):
-    tail_quantile = 0.1
-    tau = 5
-    theta = 0.1
+def boostrap_stylized_facts(
+    data: npt.ArrayLike, B: int, S: int, L: int
+) -> List[npt.NDArray]:
+    data = np.asarray(data)
+    assert data.ndim == 2
 
     def stf_ht(data, qt):
         pd, pb, nd, nb = heavy_tails.discrete_pdf(data)
@@ -74,36 +35,109 @@ def stylized_score(
     stf_vc = volatility_clustering.volatility_clustering
     stf_le = leverage_effect.leverage_effect
 
-    lu_scores, lus = _stf_score(stf_lu, real_data, syn_data, B, S, L, max_lag=1000)
-    ht_scores, hts = _stf_score(stf_ht, real_data, syn_data, B, S, L, qt=tail_quantile)
-    vc_scores, vcs = _stf_score(stf_vc, real_data, syn_data, B, S, L, max_lag=1000)
-    le_scores, les = _stf_score(stf_le, real_data, syn_data, B, S, L, max_lag=100)
-    cf_scores, cfs = _stf_score(
-        stf_cf, real_data, syn_data, B, S, L, max_lag=40, tau=tau
-    )
+    stylized_fact_list = [stf_lu, stf_ht, stf_vc, stf_le, stf_cf]
+    stylized_fact_arg_list = [
+        {"max_lag": 1000},
+        {"qt": 0.1},
+        {"max_lag": 1000},
+        {"max_lag": 60},
+        {"max_lag": 50, "tau": 5},
+    ]
 
-    gl_max = 100
-    values = np.arange(1, gl_max + 2, 1)
+    out_data = []
+    for fact, arg in zip(stylized_fact_list, stylized_fact_arg_list):
+        t_data, _ = bootstrap.boostrap_distribution(data, fact, B, S, L, **arg)
+        out_data.append(t_data)
 
-    if real_data.ndim == 3:
-        real_data = np.concatenate(real_data.tolist(), axis=1)
-    if syn_data.ndim == 3:
-        syn_data = np.concatenate(syn_data.tolist(), axis=1)
-
-    real_gains, real_loss = np.nanmean(
-        gain_loss_asymetry.gain_loss_asymmetry(real_data, max_lag=gl_max, theta=theta),
-        axis=2,
-    )
-    syn_gains, syn_loss = np.nanmean(
-        gain_loss_asymetry.gain_loss_asymmetry(syn_data, max_lag=gl_max, theta=theta),
+    gains, loss = np.nanmean(
+        gain_loss_asymetry.gain_loss_asymmetry(
+            np.cumsum(data, axis=0), max_lag=80, theta=0.1
+        ),
         axis=2,
     )
 
-    n_gains = np.sum(real_gains)
-    mu_gains = np.dot(real_gains, np.arange(1, real_gains.size + 1)) / n_gains
+    out_data.append((gains, loss))
+
+    return out_data
+
+
+def stylied_facts_from_dist(data: npt.ArrayLike) -> List[npt.NDArray]:
+    data = np.asarray(data)
+    assert data.ndim == 3
+
+    def stf_ht(data, qt):
+        pd, pb, nd, nb = heavy_tails.discrete_pdf(data)
+        (pb, _), _ = heavy_tails.heavy_tails(pd, pb, qt)
+        (nb, _), _ = heavy_tails.heavy_tails(nd, nb, qt)
+
+        return np.array([pb, nb]).reshape(-1, 1)
+
+    def stf_cf(data, tau, max_lag):
+        _, _, dll, _ = coarse_fine_volatility.coarse_fine_volatility(data, tau, max_lag)
+        return dll
+
+    stf_lu = linear_unpredictability.linear_unpredictability
+    stf_vc = volatility_clustering.volatility_clustering
+    stf_le = leverage_effect.leverage_effect
+
+    stylized_fact_list = [stf_lu, stf_ht, stf_vc, stf_le, stf_cf]
+    stylized_fact_arg_list = [
+        {"max_lag": 1000},
+        {"qt": 0.1},
+        {"max_lag": 1000},
+        {"max_lag": 60},
+        {"max_lag": 50, "tau": 5},
+    ]
+
+    out_data = []
+    for fact, arg in zip(stylized_fact_list, stylized_fact_arg_list):
+        t_data = np.asarray([np.mean(fact(d, **arg), axis=1) for d in data]).T
+        out_data.append(t_data)
+
+    log_price = np.cumsum(np.concatenate(data, axis=1), axis=0)
+    gains, loss = np.nanmean(
+        gain_loss_asymetry.gain_loss_asymmetry(log_price, max_lag=80, theta=0.1),
+        axis=2,
+    )
+
+    out_data.append((gains, loss))
+
+    return out_data
+
+
+def _stylized_score(
+    real_data_stf: List[npt.ArrayLike],
+    syn_data_stf: List[npt.ArrayLike],
+) -> List[Tuple[npt.ArrayLike, float]]:
+    out_data = []
+
+    # last fact is gain loss
+    real_data_stf = real_data_stf.copy()
+    real_gain, real_loss = real_data_stf.pop()
+
+    syn_data_stf = syn_data_stf.copy()
+    syn_gain, syn_loss = syn_data_stf.pop()
+
+    for real, syn in zip(real_data_stf, syn_data_stf):
+        n = real.shape[0]
+        assert syn.shape[0] == n
+
+        w_dist = []
+        for i in range(n):
+            std = np.nanstd(real[i, :])
+            w_dist.append(wasserstein_distance(real[i, :] / std, syn[i, :] / std))
+
+        out_data.append((np.asarray(w_dist), np.mean(w_dist)))
+
+    #######################
+    # wasserstein distance
+    #######################
+
+    # comptue stds
+    n_gains = np.sum(real_gain)
+    mu_gains = np.dot(real_gain, np.arange(1, real_gain.size + 1)) / n_gains
     std_gains = np.sqrt(
-        np.dot((np.arange(1, real_gains.size + 1) - mu_gains) ** 2, real_gains)
-        / n_gains
+        np.dot((np.arange(1, real_gain.size + 1) - mu_gains) ** 2, real_gain) / n_gains
     )
 
     n_loss = np.sum(real_loss)
@@ -112,17 +146,32 @@ def stylized_score(
         np.dot((np.arange(1, real_loss.size + 1) - mu_loss) ** 2, real_loss) / n_loss
     )
 
+    values = np.arange(1, real_gain.size + 1)
     values_g = values / std_gains
     values_l = values / std_loss
-    gain_score = wasserstein_distance(values_g, values_g, real_gains, syn_gains)
+
+    gain_score = wasserstein_distance(values_g, values_g, real_gain, syn_gain)
     loss_score = wasserstein_distance(values_l, values_l, real_loss, syn_loss)
     gl_socres = np.asarray([gain_score, loss_score])
     gls = np.mean(gl_socres)
+    out_data.append((gl_socres, gls))
 
-    scores = np.array((lus, hts, vcs, les, cfs, gls))
-    score_arrays = [lu_scores, ht_scores, vc_scores, le_scores, cf_scores]
+    ##################
+    # collect results
+    ##################
 
-    return scores, np.mean(scores), score_arrays
+    scores_lists, scores = zip(*out_data)
+
+    return np.mean(scores), scores, scores_lists
+
+
+def stylized_score(
+    real_data: npt.ArrayLike, syn_data: npt.ArrayLike, B: int, S: int, L: int
+):
+    real_stf = boostrap_stylized_facts(real_data, B, S, L)
+    syn_stf = stylied_facts_from_dist(syn_data)
+
+    return _stylized_score(real_stf, syn_stf)
 
 
 if __name__ == "__main__":
@@ -131,7 +180,7 @@ if __name__ == "__main__":
     sp500 = load_data.load_log_returns("sp500")
     smi = load_data.load_log_returns("smi")
 
-    B = 50
+    B = 5
     S = 24
     L = 4096
 
@@ -147,7 +196,7 @@ if __name__ == "__main__":
             print(f"sample garch {i}")
             garch_b.append(garch_sample)
 
-    syn_data = np.asarray(garch_b[1])
+    syn_data = np.asarray(garch_b)
 
-    scores, mu_score, _ = stylized_score(sp500, syn_data, B, S, L)
+    mu_score, scores, _ = stylized_score(sp500, syn_data, B, S, L)
     print(scores, mu_score)
