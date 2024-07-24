aec90d6296596f84bb122af03a7fd7ff4e3e3ed6
Nico Graf <ncograf@gmail.com>
2024-07-24 19:36:01 +0200

    Update run scripts with euler parameters

--- scripts/hpc_run_scripts/train_fourier_flow.sh
+++ scripts/hpc_run_scripts/train_fourier_flow.sh
@@ -2,11 +2,11 @@
 
 #SBATCH --ntasks=1
 #SBATCH --gpus=rtx_4090:1
-#SBATCH --mem-per-cpu=4096M
-#SBATCH --time=10:00:00
+#SBATCH --mem-per-cpu=10G
+#SBATCH --time=24:00:00
 #SBATCH --output=run_output.out
 #SBATCH --error=run_error.out
 
 module load eth_proxy
 
-poetry run accelerate launch --config_file accelerate.yaml --no_python ./scripts/start_agent.sh -n 5 d8rk0uig
+poetry run accelerate launch --config_file accelerate.yaml ./src/fourier_flows/train_fourier_flow.py
--- scripts/hpc_run_scripts/train_garch.sh
+++ scripts/hpc_run_scripts/train_garch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --ntasks=1
-#SBATCH --mem-per-cpu=4G
-#SBATCH --time=24:00:00
+#SBATCH --mem-per-cpu=3G
+#SBATCH --time=48:00:00
 
 poetry run python ./src/garch/grid_search_garch.py
